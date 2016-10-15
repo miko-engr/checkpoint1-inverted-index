e9d7ee7d5b1f0771ec5bcbf72fd0cf15058d81b4
Bill-Han918 <billhan918@gmail.com>
2016-10-15 11:08:35 +0100

    feature(migration): code migration from es5 to es6

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,69 +1,98 @@
-function Index(){
-    this.file = file;
-    this.readFile = function(filePath){
-       fs.readFile(filePath ,function read(data,err) {
-           if(err){
-               alert(err);
-           }
-           else{
-               file = data;
-               createIndex(file);
-           }
-       }); 
-    };
+$('document').ready(function () {
+    var wick;
+    var read = new Index();
+    $('#fileRead').on('click', function () {
+        var fileDetails = document.getElementById('filegangan').files[0];
+        console.log(fileDetails);
+        var fileName = fileDetails.name;
+        read.readFile(fileDetails, function() {
+            wick = read.files;
+            // console.log(wick.docs.length);
+        });
+        $('#load').append('<span>Your File has been successfully loaded</span>');
+    });
+    $('#index').on('click', function(){
+        alert('I am Index');
+        var numberofDocs = read.getIndex(wick);
+        // console.log(numberofDocs);
+        $('#list').attr('data-badge',numberofDocs);
+        });
+});
 
-    this.createIndex = function(file){
-        var details = [];
-        if(typeof file !=='object') {
-            alert('Your file isn\'t JSON');
-        }
-        else{
-            for(var i=0;i<Object.keys(file); i++) {
-                details.push('Document '+(i+1));
-            }
-        }
-        return details;
-    };
-    this.getIndex = function(details){
-        var fileIndex = [];
-        for(var i=0; i <details.length; i++){
+
+class Index {
+    constructor(){
+        this.files = {};
+    }
+    readFile(fileDetails,callback){
+        let fileName = fileDetails.name;
+        let _this = this;
+        let fs = new FileReader();
+        fs.readAsText(fileDetails,'UTF-8');
+        fs.onload = function(files){
+            let result = JSON.parse(files.target.result);
+            _this.files ={
+                name: fileName,
+                docs:result
+            };
+            callback();
+        };
+    }
+
+    getIndex(data){
+        let fileIndex = [];
+        for(let i=0;i<data.docs.length;i++) {
             fileIndex.push(i);
         }
-        return fileIndex;
-    };
-    this.searchIndex = function(){
-        var reff = new showOccurences();
+        return fileIndex.length;
+    }
+    createIndex(document){
+        let details = [];
+        if (typeof document !=='object') {
+            return false;
+        }
+        else{
+            for(let i=0;i<Object.keys(document);i++){
+                details.push('Document '+(i+1));
+            }
+            return details;
+        }
+    }
+    searchIndex(terms){
+        let reff = new showOccurences();
         terms.trim();
-	if(typeof terms !== 'string' || terms ==='' || (terms.trim())==='') {
-		return('We didn\'t forget to check though!!!')
-	}
-	else{
-		var words = terms.split(' ');
-		for(var i=0; i<words.length; i++){
-			if(reff[words[i]]===undefined){
-				return (words[i] +' : ' +'Not Found!!!!');
-			}
-			else{
-				return (words[i] +' : ' +reff[words[i]]);
-			}
-		}
-	}
-    };
-    var showOccurences = function(){
-        var merge = [];
-        var wordOccurrence ={};
+        if (typeof terms !== 'string' || terms === '' || (terms.trim()) === '') {
+            console.log('We didn\'t forget to check though!!!');
+        }
+        else {
+            let words = terms.split(' ');
+            for (let i = 0; i < words.length; i++) {
+                if (reff[words[i]] === undefined) {
+                    return (words[i] + ' : ' + 'Not Found!!!!');
+                }
+                else {
+                    return (words[i] + ' : ' + reff[words[i]]);
+                }
+            }
+        }
+    }
 
-        for(var i=0; i<Object.keys(file);i++){
-            merge.push(file[i].text.split(' '));
-            var keywords = [].concat.apply([], merge);
-            var mySet = new Set(keywords);
-            for (let key of mySet.keys()){
-				if (!wordOccurrence.hasOwnProperty(key)){
-					wordOccurrence[key] = [];
-				}
-				wordOccurrence[key].push('Document '+(1+i));
-		}
+}
+
+var showOccurences = function () {
+    var merge = [];
+    var wordOccurrence = {};
+
+    for (var i = 0; i < Object.keys(file); i++) {
+        merge.push(file[i].text.split(' '));
+        var keywords = [].concat.apply([], merge);
+        var mySet = new Set(keywords);
+        for (let key of mySet.keys()) {
+            if (!wordOccurrence.hasOwnProperty(key)) {
+                wordOccurrence[key] = [];
+                }
+                wordOccurrence[key].push('Document ' + (1 + i));
+            }
         }
         return wordOccurrence;
     };
-}
