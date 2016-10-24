8b7948b23388945f587d185833c6c5000109e6b0
Bill-Han918 <billhan918@gmail.com>
2016-10-24 17:51:56 +0100

    Removal of duplication of functionality unnecessary function removal

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -6,21 +6,31 @@ class Index {
         for(let i=0;i<data.docs.length;i++) {
             fileIndex.push(i);
         }
-        return fileIndex.length;
+        return fileIndex;
     }
-    createIndex(document){
-        let contents = document.docs;
-        if (typeof document !=='object') {
-            console.log('Aww, Snap!!!');
-            return false;
-        }
-        else{
-            return contents;
-        }
+    createIndex(file){
+            let wordOccurrence = {};
+            let merge = [];
+            let keywords =[];
+            let mySet;
+            for (let i=0; i <file.length; i++){
+                let clean = tokenize(file[i].text);
+                merge.push(clean.split(' '));
+                keywords = [].concat.apply([], merge);
+                mySet = new Set(keywords);
+                for (let key of mySet.keys()) {
+                    if (clean.includes(key)){
+                        if (!wordOccurrence.hasOwnProperty(key)){
+                            wordOccurrence[key] = [];
+                        }
+                        wordOccurrence[key].push(i);
+                    }
+                }
+            }
+            return wordOccurrence;
     }
-    searchIndex(terms){
-        let reff = new showOccurrences();
-        terms.trim();
+    searchIndex(terms,reff){
+        let result = {};
         if (typeof terms !== 'string' || terms === '' || (terms.trim()) === '') {
             console.log('We didn\'t forget to check though!!!');
         }
@@ -28,39 +38,22 @@ class Index {
             let words = terms.split(' ');
             for (let i = 0; i < words.length; i++) {
                 if (reff[words[i]] === undefined) {
-                    return (words[i] + ' : ' + 'Not Found!!!!');
+                    console.log(words[i] + ' : ' + 'Not Found!!!!');
                 }
                 else {
-                    return (words[i] + ' : ' + reff[words[i]]);
+                    let key = words[i];
+                    let value = reff[words[i]];
+                    result[key] = value;
                 }
             }
         }
+        return result;
     }
 
 }
 
-let showOccurrences = function (file) {
-    let wordOccurrence = {};
-	let merge = [];
-	for (let i=0; i <file.length; i++){
-        let clean = tokenize(file[i].text);
-		merge.push(clean.split(' '));
-		let keywords = [].concat.apply([], merge);
-		let mySet = new Set(keywords);
-		for (let key of mySet.keys()) {
-			if (clean.includes(key)){
-				if (!wordOccurrence.hasOwnProperty(key)){
-					wordOccurrence[key] = [];
-				}
-				wordOccurrence[key].push('Document '+(1+i));
-			}
-		}
-	}
-	return wordOccurrence;
-    };
-
     function tokenize(tokens) {
-        var newToken = tokens.replace(/[^a-z0-9]+/gi, ' ').toLowerCase();
+        var newToken = tokens.replace(/[^a-z0-9]+/gi, ' ').trim().toLowerCase();
         //send out the new token
         return newToken;
     }
