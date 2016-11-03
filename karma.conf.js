0a0ec54ac023008aff92bf68e84fc8b806090be2
Bill-Han918 <billhan918@gmail.com>
2016-11-03 12:21:07 +0100

    Verbose reporter fix
    Dependency update in package.json
    karma config update

--- karma.conf.js
+++ karma.conf.js
@@ -36,8 +36,8 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress','coverage'],
-
+    reporters: ['coverage', 'verbose'],
+    
     coverageReporter: {
       type: 'lcov',
       dir: 'coverage/'
